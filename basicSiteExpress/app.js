41323ad2d391d1cc8584454f0f3a075e59635e58
Mikhail <mikhailyu@hotmail.co.nz>
2023-03-26 14:34:14 +1300

    redid with express

--- basicSiteExpress/app.js
+++ basicSiteExpress/app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const path = require("path");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/html/index.html"));
+  //__dirname : It will resolve to your project folder.
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/about.html", function (req, res) {
+  res.sendFile(path.join(__dirname + "/html/about.html"));
 });
 
-module.exports = app;
+app.get("/contact-me.html", function (req, res) {
+  res.sendFile(path.join(__dirname + "/html/contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname + "/html/404.html"));
+});
+
+// app.get gets the url but app.use is a general func
+// app.use is used here to redirect any link that isn't in app.get to 404.html
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
